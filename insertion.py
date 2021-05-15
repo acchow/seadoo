dbcfd6ea5fe559343e99090b4865e949f6efea06
acchow <amandachow898@gmail.com>
2021-05-15 02:18:44 -0400

    minor changes, added comments

--- insertion.py
+++ insertion.py
@@ -1,9 +1,19 @@
-'''
- August 2020
- By Amanda Chow
- This program either inserts a new theory to a chain decomposition
- or searches for an equivalent theory
-'''
+"""
+bug: existing theories are not updated/added to new chains created by the new theory entered
+potentially change algo to :
+
+1)
+checking chains from the final theory, down toward the root theory
+once it stops (inconsistent) , copy all the theories traversed so far and put them into a new chain w/ the new theory
+
+2)
+check if consistent or not with the root and trunk theories
+if consistent,
+    continue w/ implemented binary search
+if not consistent,
+    start traversing from the end of the chain to check if a new chain should be formed w new theory
+
+"""
 
 import relationship
 import pandas as pd
@@ -26,7 +36,7 @@ def find_position(chain, new_t, low, high, insert=False):
 
     # middle value
     if "equivalent" in compare_mid:
-        # theory is already there
+        # theory already exists in the chain. only the names are checked (not the contents/axioms)
         if new_t == chain[mid]:
             return -1
         # found an equivalent theory
@@ -88,20 +98,22 @@ def main(csv_file, new_t, function):
         print("your theory is inconsistent")
     else:
         # update each chain
-        for i, chain in enumerate(chains_list):
-            condition = relationship.main(new_t, chain[0] + ".in")
-            if "inconsistent" in condition or "independent" in condition:
-                continue
-            else:
-                if function == 1:
-                    new_chain = insertion(chain, input_chains[i], new_t)
-                    some_t = new_t.replace(".in", "")
-                    if some_t in new_chain:
-                        inserted = True
-                    print(inserted)
-                elif function == 2:
-                    print(search(input_chains[i], new_t))
-                    inserted = True
+
+        #for i, chain in enumerate(chains_list):
+         #   condition = relationship.main(new_t, chain[0] + ".in")
+          #  if "inconsistent" in condition or "independent" in condition:
+           #     continue
+            #else:
+
+        if function == 1:
+            new_chain = insertion(chain, input_chains[i], new_t)
+            some_t = new_t.replace(".in", "")
+            if some_t in new_chain:
+                inserted = True
+            print(inserted)
+        elif function == 2:
+            print(search(input_chains[i], new_t))
+            inserted = True
 
         if inserted is False:
             # create a new chain
@@ -123,5 +135,5 @@ def complete_insertion(csv_file):
 
 
 # 1 for insert, 2 for search
-#main("semilinear-orderings.csv", "quasiorder.in", 1)
+main("semilinear-orderings.csv", "lower_bound.in", 1)
 #complete_insertion("between.csv")
