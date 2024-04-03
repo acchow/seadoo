3f760ddae2ce0c7ca7bd4d3d903cce4d30dc9dc9
acchow <amandachow898@gmail.com>
2024-04-02 22:24:06 -0400

    add relation name column to db

--- db/create.sql
+++ db/create.sql
@@ -2,6 +2,7 @@ CREATE TABLE hierarchies (
 	hierarchy_name varchar(255),
 	nondecomp_hierarchies varchar(255),
 	num_prim_relations int NOT NULL,
+	relation_name varchar(255),
 	root_theory varchar(255),
 	PRIMARY KEY (hierarchy_name)
 );
--- db/insert.sql
+++ db/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO hierarchies VALUES 
-    ('graphs', '', 1, 'undirected_graph'), 
-    ('mereograph', 'graphs,orderings', 2, ''), 
-    ('orderings', '', 1, 'quasi_order'), 
-    ('subgraph', 'graphs,graphs', 2, ''),
-    ('subposet', 'orderings,orderings', 2, '')
+    ('graphs', '', 1, 'adj', 'undirected_graph'), 
+    ('mereograph', 'graphs,orderings', 2, '', ''), 
+    ('orderings', '', 1, 'leq', 'quasi_order'), 
+    ('subgraph', 'graphs,graphs', 2, '',''),
+    ('subposet', 'orderings,orderings', 2, '','')
 ;
